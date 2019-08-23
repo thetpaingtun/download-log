84c68ca683581f0c56971382f82a0671b3afef6f
thet <thet@innov8tif.com>
2019-08-23 18:20:37 +0800

    added puppeteer and related code;

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
+/* eslint-disable comma-dangle */
 /* eslint-disable object-curly-spacing */
 const { Command, flags } = require('@oclif/command')
 const fs = require('fs-extra')
 const path = require('path')
-
+const puppeteer = require('puppeteer')
 
 // eslint-disable-next-line padded-blocks
 class DlCommand extends Command {
@@ -13,9 +14,45 @@ class DlCommand extends Command {
     const name = flags.name || 'world'
     this.log(`hello ${name} from .\\src\\index.js`)
 
-    const configJson = await fs.readJson(path.join(this.config.configDir, 'config.json'))
-    this.log(`config => ${configJson.path}`)
+    // const configJson = await fs.readJson(path.join(this.config.configDir, 'config.json'))
+    // this.log(`config => ${configJson.path}`)
 
+    this.browse()
+  }
+
+  async browse() {
+    const browser = await puppeteer.launch({ headless: false })
+    const page = await browser.newPage()
+    await page.goto('http://innov8tifip.ddns.net:9086/valyou/admin/login')
+
+    await page.type('#username', 'admin')
+
+    const edts = await page.$x('//*[@id="main"]/form/input[2]')
+    const edtPass = edts[0]
+    await edtPass.type('admin')
+
+
+    const btns = await page.$x('//*[@id="main"]/form/button')
+    const btnLogin = btns[0]
+
+    await Promise.all([
+      page.waitForNavigation(),
+      btnLogin.click()
+    ])
+
+    // await btnLogin.click()
+
+    await page.goto('http://innov8tifip.ddns.net:9086/valyou/admin/log/list')
+
+    const links = await page.$x("//a[contains(., 'valyou.log')]")
+    const logLinks = links[0]
+
+    await logLinks.click()
+
+    await page.screenshot({ path: 'example.png' })
+
+
+    await browser.close()
   }
 }
 
