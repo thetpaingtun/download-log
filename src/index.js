bff9d2053fe73c91a3470aff47163a440179b706
thet <thet@innov8tif.com>
2019-08-28 18:07:03 +0800

    added some code

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const { Command, flags } = require('@oclif/command')
 const fs = require('fs-extra')
 const path = require('path')
 const puppeteer = require('puppeteer')
+const shell = require('shelljs')
 
 // eslint-disable-next-line padded-blocks
 class DlCommand extends Command {
@@ -12,16 +13,29 @@ class DlCommand extends Command {
     const { flags } = this.parse(DlCommand)
     const { args } = this.parse(DlCommand)
     const name = flags.name || 'world'
-    this.log(`hello ${name} from .\\src\\index.js`)
+    this.log(`hello ${name} from .\\src\\index1.js`)
 
     // const configJson = await fs.readJson(path.join(this.config.configDir, 'config.json'))
     // this.log(`config => ${configJson.path}`)
 
+
+
+    // shell.exec('"C:\\Program Files\\Sublime Text 3\\subl.exe" C:\\Users\\user\\Desktop\\asoft.txt')
+
+
+    // this.log('completed')
+
+
+
+
+
+
     this.browse()
   }
 
+
   async browse() {
-    const browser = await puppeteer.launch({ headless: false })
+    const browser = await puppeteer.launch({ headless: true })
     const page = await browser.newPage()
     await page.goto('http://innov8tifip.ddns.net:9086/valyou/admin/login')
 
@@ -40,14 +54,36 @@ class DlCommand extends Command {
       btnLogin.click()
     ])
 
-    // await btnLogin.click()
-
     await page.goto('http://innov8tifip.ddns.net:9086/valyou/admin/log/list')
+
+
+    await page.setRequestInterception(true)
+
+    await page.on('request', request => {
+      if (request.resourceType() === 'document') {
+
+
+
+      }
+
+      request.continue()
+    })
+
+    await page.on('response', (response) => {
+
+    })
+
+    await page._client.send('Page.setDownloadBehavior', {
+      behavior: 'allow',
+      downloadPath: './'
+    });
 
     const links = await page.$x("//a[contains(., 'valyou.log')]")
     const logLinks = links[0]
 
     await logLinks.click()
+
+    await page.waitForNavigation({waitUntil:'networkidle2'})
 
     await page.screenshot({ path: 'example.png' })
 
